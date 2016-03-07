62193801d14e7359743b1aef3989c592a7104066
Cullin <cullin_lam@student.uml.edu>
2016-03-07 15:00:43 -0500

    Latest commit from cullin

--- ClockWork/client/events/event-detail/event-detail.component.js
+++ ClockWork/client/events/event-detail/event-detail.component.js
@@ -13,7 +13,6 @@ angular
         $reactive(this).attach($scope);
 
         var event_id = $stateParams.eventId;
-        console.log("LOOOOOOOOOOOOOOOOOOOOOOOOOK");
         console.log(event_id);
         //hardcode user's name until we get the User collections up
         this.name = "ClockWork Master";
