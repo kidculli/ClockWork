11dd54f64c4b145dbcf34ef8fc6d2002be516c61
sunmaximus <son_nguyen@student.uml.edu>
2016-03-08 02:04:08 -0500

    Fixed??????

---
+++ ClockWork/mobile-config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Cullin on 2/28/16.
+ *
+ * file: mobile-config.js
+ *
+ * Sets configurations for mobile platforms for Cordova
+ *
+ * see https://docs.meteor.com/#/full/App-info
+ */
+
+//- src http://stackoverflow.com/questions/29279287/in-meteor-app-how-to-add-orgin-in-cordova-config-xml
+// allow access to all domains; security risk but ok for now
+App.accessRule('*');
