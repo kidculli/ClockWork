432faf4edf9b7e57454019696c690146b53518cc
sunmaximus <son_nguyen@student.uml.edu>
2016-03-08 08:39:32 -0500

    Test this upload
    eerer

--- ClockWork/client/modal/addEvent.component.js
+++ ClockWork/client/modal/addEvent.component.js
@@ -1,7 +1,4 @@
 /**
- * Created by Son on 2/23/2016.
- */
-/**
  * Author: Son Nguyen
  *
  * file: event-feed.component.js
@@ -24,183 +21,184 @@
 angular
     .module('ClockWork').controller('addEventCtrl', function($scope, $reactive) {
 
-        //uses 'this' instead of '$scope' because of controllerAs.
-        $reactive(this).attach($scope);
+    //uses 'this' instead of '$scope' because of controllerAs.
+    $reactive(this).attach($scope);
 
-        // This will be use to trigger ng-show for custom validation on ng-click
-        var check_time = false;
-        var check_name = false;
-        var check_description = false;
-        var check_event_type = false;
-        var check_capacity = false;
+    // This will be use to trigger ng-show for custom validation on ng-click
+    var check_time = false;
+    var check_name = false;
+    var check_description = false;
+    var check_event_type = false;
+    var check_capacity = false;
 
-        //declaring empty eventS
-        this.newEvent = {};
+    //declaring empty eventS
+    this.newEvent = {};
 
-        //this.events = $meteor.collection(ClockWork);
-        this.helpers({
-            events: function(){
-                return Events.find({});
-            }
-        });
+    //this.events = $meteor.collection(ClockWork);
+    this.helpers({
+        events: function(){
+            return Events.find({});
+        }
+    });
 
-        //use this variable to store the selected value in the timePicker.
-        var expire_time= 0;
+    //use this variable to store the selected value in the timePicker.
+    var expire_time= 0;
 
-        //Edit the dictionary for user's timePicker package preference.
-        //For more information visit: https://github.com/rajeshwarpatlolla/ionic-timepicker
-        //TimePicker uses Unix time stamp
-        this.timePickerObject = {
-            inputEpochTime: 0, //((new Date()).getHours() * 60 * 60),  //Optional
-            step: 5,  //Optional
-            format: 24,  //Optional
-            titleLabel: '24-hour Format',  //Optional
-            setLabel: 'Set',  //Optional
-            closeLabel: 'Close',  //Optional
-            setButtonType: 'button-positive',  //Optional
-            closeButtonType: 'button-stable',  //Optional
-            callback: function (val) {    //Mandatory
-                timePickerCallback(val);
-            }
-        };
+    //Edit the dictionary for user's timePicker package preference.
+    //For more information visit: https://github.com/rajeshwarpatlolla/ionic-timepicker
+    //TimePicker uses Unix time stamp
+    this.timePickerObject = {
+        inputEpochTime: 0, //((new Date()).getHours() * 60 * 60),  //Optional
+        step: 5,  //Optional
+        format: 24,  //Optional
+        titleLabel: '24-hour Format',  //Optional
+        setLabel: 'Set',  //Optional
+        closeLabel: 'Close',  //Optional
+        setButtonType: 'button-positive',  //Optional
+        closeButtonType: 'button-stable',  //Optional
+        callback: function (val) {    //Mandatory
+            timePickerCallback(val);
+        }
+    };
 
-        function timePickerCallback(val) {
-            if (typeof (val) === 'undefined') {
-                console.log('Time not selected');
+    function timePickerCallback(val) {
+        if (typeof (val) === 'undefined') {
+            console.log('Time not selected');
 
-                // Not part of the timePicker package. check_time will be use to check if the user never selected a time.
-                check_time = true
-                //console.log(".00", check_time);
+            // Not part of the timePicker package. check_time will be use to check if the user never selected a time.
+            check_time = true
+            //console.log(".00", check_time);
 
-            } else {
-                var selectedTime = new Date(val * 1000);
-                console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), ':', selectedTime.getUTCMinutes(), 'in UTC');
+        } else {
+            var selectedTime = new Date(val * 1000);
+            console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), ':', selectedTime.getUTCMinutes(), 'in UTC');
 
-                //This is not from the timePicker package. Use this variable to append into this.newEvent
-                expire_time = val*1000 + (new Date).getTime();  //Have to multiply 1000 to the epoch time.
-                check_time = false;
-                //console.log(".11", check_time);
-            }
-
-            // This is also not form the time picker package.
-            // Displace ng-show validation for timePicker if the user selected 00. 0 time does not make logic sense.
-            if(val == 0){
-                check_time = true;
-                //console.log(".22", check_time);
-            }
+            //This is not from the timePicker package. Use this variable to append into this.newEvent
+            expire_time = val*1000 + (new Date).getTime();  //Have to multiply 1000 to the epoch time.
+            check_time = false;
+            //console.log(".11", check_time);
         }
 
-        /* Description: This function validate if the form and model are valid on submission.
-        *               Then it'll reinitialize everything, hide and reset the form. Else, trigger all the check
-        *               variables on ng-show.
-        *  Variable:
-        *       valid - type(bool): True if the whole form is valid on submission. Else false.
-        *       form - type(form object): form is a variable that store the "name" of a form.
-        *       name, description, event_type, and capacity - type(ng-model object): value of the model in the form.*/
-        this.addEvent = function(valid, form, name, description, event_type, capacity){
+        // This is also not form the time picker package.
+        // Displace ng-show validation for timePicker if the user selected 00. 0 time does not make logic sense.
+        if(val == 0){
+            check_time = true;
+            //console.log(".22", check_time);
+        }
+    }
 
-            if(valid === true && check_time === false && typeof(name) != 'undefined' &&
-                typeof(description) != 'undefined' && typeof(event_type) != 'undefined' && typeof(capacity) != 'undefined') {
+    /* Description: This function validate if the form and model are valid on submission.
+     *               Then it'll reinitialize everything, hide and reset the form. Else, trigger all the check
+     *               variables on ng-show.
+     *  Variable:
+     *       valid - type(bool): True if the whole form is valid on submission. Else false.
+     *       form - type(form object): form is a variable that store the "name" of a form.
+     *       name, description, event_type, and capacity - type(ng-model object): value of the model in the form.*/
+    this.addEvent = function(valid, form, name, description, event_type, capacity){
 
-                ////convert cap to int if not 10+
-                if (this.newEvent['cap'].search('\\++') == -1) {
-                    this.newEvent['cap'] = parseInt(this.newEvent['cap'], 10);
-                }
-                // append time created to newEvent date format is number milliseconds since epoch
-                this.newEvent['time_created'] = new Date().getTime();
-                //append expire time from timePicker into newEvent
-                this.newEvent['time_expire'] = expire_time;
-                // initialize current fill of event to 1
-                this.newEvent['fill'] = 1;
-                // hardcode user id, it will be changed to Meteor.UserId() when sign in implemented
-                this.newEvent['owner'] = 'ClockWorkMaster';
-                // add empty array for attendees
-                this.newEvent['attendees'] = [];
-                // add empty object for loc
-                this.newEvent['loc'] = {};
-                // add expired field
-                this.newEvent['expired'] = false;
-                Events.insert(this.newEvent);
-                console.log("Added Event:", this.newEvent);
-                //reset selected time to 0
-                expire_time = 0;
-                //reset newEvent
-                this.newEvent = {};
+        if(valid === true && check_time === false && typeof(name) != 'undefined' &&
+            typeof(description) != 'undefined' && typeof(event_type) != 'undefined' && typeof(capacity) != 'undefined') {
 
-                // Reset all check variable to false since form was valid on submission
-                check_time = false;
-                check_name = false;
-                check_description = false;
-                check_event_type = false;
-                check_capacity = false;
-
-                $scope.modal.hide();
-
-                /* Reset Form */
-                form.$setPristine();
-                form.$setUntouched();
-
+            ////convert cap to int if not 10+
+            if (this.newEvent['cap'].search('\\++') == -1) {
+                this.newEvent['cap'] = parseInt(this.newEvent['cap'], 10);
             }
-            else{
-                check_time = true;
-                check_name = true;
-                check_description = true;
-                check_event_type = true;
-                check_capacity = true;
-                $scope.modal.show();
-            }
+            // append time created to newEvent date format is number milliseconds since epoch
+            this.newEvent['time_created'] = new Date().getTime();
+            //append expire time from timePicker into newEvent
+            this.newEvent['time_expire'] = expire_time;
+            // initialize current fill of event to 1
+            this.newEvent['fill'] = 1;
+            // hardcode user id, it will be changed to Meteor.UserId() when sign in implemented
+            this.newEvent['owner'] = 'ClockWorkMaster';
+            // add empty array for attendees
+            this.newEvent['attendees'] = [];
+            // add empty object for loc
+            this.newEvent['loc'] = {};
+            // add expired field
+            this.newEvent['expired'] = false;
+            Events.insert(this.newEvent);
+            console.log("Added Event:", this.newEvent);
+            //reset selected time to 0
+            expire_time = 0;
+            //reset newEvent
+            this.newEvent = {};
 
-        };
+            // Reset all check variable to false since form was valid on submission
+            check_time = false;
+            check_name = false;
+            check_description = false;
+            check_event_type = false;
+            check_capacity = false;
 
-        /* Custom Validation:
-        *  Basic rule for all the check functions. Since all check variables are initialize as false.
-        *  If any of the check variable equal to true, then tt will trigger ng-show on the html*/
+            $scope.modal.hide();
 
-        this.checkName = function(){
-            if(check_name == true ){
-                return true;
-            }else {
-                return false;
-            }
+            /* Reset Form */
+            form.$setPristine();
+            form.$setUntouched();
+
+        }
+        else{
+            check_time = true;
+            check_name = true;
+            check_description = true;
+            check_event_type = true;
+            check_capacity = true;
+            $scope.modal.show();
         }
 
-        this.checkDescription = function(){
-            if(check_description == true){
-                return true;
-            }else {
-                return false;
-            }
-        }
+    };
 
-        this.checkType = function(){
-            if(check_event_type == true){
+    /* Custom Validation:
+     *  Basic rule for all the check functions. Since all check variables are initialize as false.
+     *  If any of the check variable equal to true, then tt will trigger ng-show on the html*/
 
-                return true;
-            }else {
-                return false;
-            }
+    this.checkName = function(){
+        if(check_name == true ){
+            return true;
+        }else {
+            return false;
         }
+    }
 
-        this.checkCapacity = function(){
-            if(check_capacity == true){
-                return true;
-            }else {
-                return false;
-            }
+    this.checkDescription = function(){
+        if(check_description == true){
+            return true;
+        }else {
+            return false;
         }
+    }
 
-        // Check this website out for the idea http://codepen.io/sevilayha/pen/HnxkJ
-        // Displace ng-show to validate if timePicker have not been selected.
-        this.checkTime = function(){
-            if(check_time == true){
-                // want to return "true" so ng-show will always show up in the html.
-                return true;
-            }
-            else{
-                return false;
-            }
+    this.checkType = function(){
+        if(check_event_type == true){
+
+            return true;
+        }else {
+            return false;
         }
-        /* End of Custom Validation*/
+    }
+
+    this.checkCapacity = function(){
+        if(check_capacity == true){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    // Check this website out for the idea http://codepen.io/sevilayha/pen/HnxkJ
+    // Displace ng-show to validate if timePicker have not been selected.
+    this.checkTime = function(){
+        if(check_time == true){
+            // want to return "true" so ng-show will always show up in the html.
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    /* End of Custom Validation*/
+
     });
 
 
