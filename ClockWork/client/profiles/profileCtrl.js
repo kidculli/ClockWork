f783e17a587673246485ae16d3f40601e4c24f33
Cullin <cullin_lam@student.uml.edu>
2016-04-02 04:31:44 -0400

    Added user profile view still need to fix

--- ClockWork/client/profiles/profileCtrl.js
+++ ClockWork/client/profiles/profileCtrl.js
@@ -8,11 +8,40 @@ angular.module("ClockWork").controller('profileCtrl',function($scope,$reactive,$
     // attach reactive context to this object
     $reactive(this).attach($scope);
 
-    var user_id = $stateParams.userId
+    console.log($stateParams);
+    var user_id = $stateParams.userId;
+    console.log(user_id);
     // get user info
-    this.helpers({profile:function(){
-        Users.findOne({_id:user_id})
-    }});
+    this.helpers(
+        {
+            profile_info:function() {
+                return Users.findOne({username:user_id})
+            },
+
+            attending:function(){
+                console.log('running attending helper');
+                return Events.findOne({attendees:user_id,expired:false});
+            },
+
+            attended:function(){
+                return Events.find({attendees:user_id,expired:true})
+            },
+            hide_attending:function(){
+                var obj = Events.findOne({attendees:user_id, expired:false});
+                if (obj.length == 0) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+                //if (len > 0 ){
+                //    return false;
+                //}
+                //else {
+                //    return true;
+                //}
+            }
+        });
     // test
-    this.bio = "Just a young nigga who wanna chill with the bois. Roll up the dubies naww mean."
+    this.bio = "I like to Dance and Code and stuff. On this to chill."
 });
